cf85162fb89cad12b4a63416658468aadd5d4751
Lodi Dodevska <lodi.dodevska@sorsix.com>
2020-04-28 23:30:39 +0200

    Added regular expressions for rtvslo

--- run-extractor.py
+++ run-extractor.py
@@ -5,4 +5,6 @@ if __name__ == '__main__':
     # regex_extractor.extract_contents("./WebPages/overstock.com/jewelry01.html")
     # regex_extractor.extract_contents("./WebPages/overstock.com/jewelry02.html")
     # xpath_extractor.extract_content("./WebPages/overstock.com/jewelry01.html")
-    prettify.run("./WebPages/overstock.com/jewelry01.html", "./WebPages/overstock.com/jewelry02.html")
+    # prettify.run("./WebPages/overstock.com/jewelry01.html", "./WebPages/overstock.com/jewelry02.html")
+    regex_extractor.extract_contents_rtvslo(
+        "./WebPages/rtvslo.si/Audi A6 50 TDI quattro_ nemir v premijskem razredu - RTVSLO.si.html")
